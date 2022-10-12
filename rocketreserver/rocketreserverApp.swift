c802f4abaff762aedf791cac7eb819ca7e8217c6
Lucas Leadbetter <5595530+lleadbet@users.noreply.github.com>
2022-10-12 11:21:23 -0400

    Initial Commit

---
+++ rocketreserver/rocketreserverApp.swift
@@ -0,0 +1,17 @@
+//
+//  rocketreserverApp.swift
+//  rocketreserver
+//
+//  Created by lleadbetter on 10/12/22.
+//
+
+import SwiftUI
+
+@main
+struct rocketreserverApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
